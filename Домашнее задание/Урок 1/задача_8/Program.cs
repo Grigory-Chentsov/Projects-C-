7b1d6785644a72f9ca847b4f6c03a88dc3aa60fb
Grigory-Chentsov <121394273+Grigory-Chentsov@users.noreply.github.com>
2023-01-15 22:39:02 +0300

    Update and rename Homework/Lesson_1/task_8/Program.cs to Домашнее задание/Урок 1/задача_8/Program.cs

--- Домашнее задание/Урок 1/задача_8/Program.cs
+++ Домашнее задание/Урок 1/задача_8/Program.cs
@@ -1,6 +1,5 @@
 ﻿// Показать вторую цифру трёхзначного числа
 
 Console.Write("Введи трехзначное число: ");
-//int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string? stringNumber = Convert.ToString(Console.ReadLine()!);
 Console.WriteLine("Вторая цифра числа -> " + stringNumber[1]);
