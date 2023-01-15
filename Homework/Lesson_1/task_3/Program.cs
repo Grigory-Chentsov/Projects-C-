95fa7e4848ab1ec74396d98bf1cfd38c3d8d92bc
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-15 20:35:58 +0300

    Added task_3

---
+++ Homework/Lesson_1/task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// По заданному номеру дня недели вывести его название
+
+string[] days = {"Понедельник", "Вторник", "Среда",
+               "Четверг", "Пятница", "Суббота", "Воскресенье"};
+
+Console.WriteLine("Введите число от 1 до 7.");
+int number_user = Convert.ToInt32(Console.ReadLine()) - 1;
+
+Console.WriteLine(days[number_user]);
