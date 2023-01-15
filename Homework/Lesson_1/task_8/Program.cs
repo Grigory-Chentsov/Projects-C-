964db16de437e33f5e473669885bf5eb4a4fcb7a
Grigory-Chentsov <121394273+Grigory-Chentsov@users.noreply.github.com>
2023-01-15 22:48:10 +0300

    Add files via upload

---
+++ Homework/Lesson_1/task_8/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введи трехзначное число: ");
+//int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string? stringNumber = Convert.ToString(Console.ReadLine()!);
+Console.WriteLine("Вторая цифра числа -> " + stringNumber[1]);
