9cc438b15d5ec9faad58b2c81fb450e40a42769a
Grigory-Chentsov <121394273+Grigory-Chentsov@users.noreply.github.com>
2023-01-15 22:50:22 +0300

    Delete Program.cs

--- Домашнее задание/Урок 1/задача_8/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// Показать вторую цифру трёхзначного числа
-
-Console.Write("Введи трехзначное число: ");
-string? stringNumber = Convert.ToString(Console.ReadLine()!);
-Console.WriteLine("Вторая цифра числа -> " + stringNumber[1]);
