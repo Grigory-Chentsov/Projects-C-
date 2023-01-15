9fd45b0eee381c1c17925eb2f7a7f03a9e6a9e02
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-15 21:53:24 +0300

    Added task_6

---
+++ Homework/Lesson_1/task_6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите ваше число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+        count += 2;
+    }
+    else
+    {
+        count += 1;
+    }
+}
