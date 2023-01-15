d36d4e77c854cabef29a3bba072b7b90e8a8a632
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-15 21:31:09 +0300

    Added task_4

---
+++ Homework/Lesson_1/task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти максимальное из трех чисел
+
+Console.WriteLine("Введите первое число: ");
+int one = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int two = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int three = Convert.ToInt32(Console.ReadLine());
+
+int max = one;
+
+if (one > max)
+    max = one;
+if (two > max)
+    max = two;
+if (three > max)
+    max = three;
+Console.WriteLine(max + " максимальное число.");
