f062603db006b994fe39e475ba6dfae705651ac8
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-15 21:50:34 +0300

    Added task_5

---
+++ Homework/Lesson_1/task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите ваше число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine(n + " является чётным.");
+}
+else
+{
+    Console.WriteLine(n + " является нечётным.");
+}
