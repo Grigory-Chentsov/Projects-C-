7858f244637d5db96a4671c0446cac42a935c6b8
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-16 23:42:28 +0300

    Added task_10

---
+++ Homework/Lesson_1/task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int new_number = (number / 100) * 10 + (number % 10);
+Console.WriteLine(new_number);
+
