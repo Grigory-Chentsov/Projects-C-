d4c6bc7d5d179c48eff4fc39a90c79e98245057b
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-16 23:02:17 +0300

    Added task_9

---
+++ Homework/Lesson_1/task_9/Program.cs
@@ -0,0 +1,20 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10, 99);
+
+int first_number = number / 10;
+int second_number = number % 10;
+
+if (first_number > second_number)
+{
+    Console.WriteLine(first_number);
+}
+else
+{
+    Console.WriteLine(second_number);
+}
+
+
+Console.WriteLine(number);
+
+
