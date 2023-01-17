f36419dfde139813a6a5b298421bf5b8fa74a260
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-17 23:20:07 +0300

    Added task_12

---
+++ Homework/Lesson_1/task_12/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int n = 846;
+int len = 0;
+int number = n;
+
+while (number > 0)
+{
+    number = number / 10;
+    len++;
+
+}
+
+if (len == 3)
+    Console.WriteLine(n % 10);
+else
+    Console.WriteLine("Не трёхзначное число.");
+
