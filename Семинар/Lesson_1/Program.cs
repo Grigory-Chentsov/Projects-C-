59f7723a24c276930411071b9b3dd76f922eec71
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-17 23:30:46 +0300

    Add changes new file:   "\320\241\320\265\320\274\320\270\320\275\320\260\321\200/.gitignore" new file:   "\320\241\320\265\320\274\320\270\320\275\320\260\321\200/Lesson_1/Program.cs" new file:   "\320\241\320\265\320\274\320\270\320\275\320\260\321\200/Lesson_1/\320\241\320\265\320\274\320\270\320\275\320\260\321\200.csproj" new file:   "\320\241\320\265\320\274\320\270\320\275\320\260\321\200/Lesson_2/Lesson_2.csproj" new file:   "\320\241\320\265\320\274\320\270\320\275\320\260\321\200/Lesson_2/Program.cs"

---
+++ Семинар/Lesson_1/Program.cs
@@ -0,0 +1,86 @@
+﻿// Поиск квадрата числа Зал 2
+
+// Console.WriteLine("Введите ваше число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int number = int.Parse(Console.ReadLine());
+// int square = (int)Math.Pow(number, 2);
+// int square = number * number;
+// Console.WriteLine(square);
+
+
+// Задача 2:
+/* Напишите программу, которая на вход принимает два числа и выдаёт,
+ какое число большее, а какое меньшее.*/
+
+// Console.WriteLine("Введите первое число: ");
+// int one = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число: ");
+// int two = Convert.ToInt32(Console.ReadLine());
+
+// if (one > two)
+//     Console.WriteLine(one + " больше.");
+// else
+//     Console.WriteLine(two + " больше.");
+
+
+// Задача 4:
+/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+
+// Console.WriteLine("Введите первое число: ");
+// int one = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число: ");
+// int two = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите третье число: ");
+// int three = Convert.ToInt32(Console.ReadLine());
+
+// int max = one;
+
+// if (one > max)
+//     max = one;
+// if (two > max)
+//     max = two;
+// if (three > max)
+//     max = three;
+// Console.WriteLine(max + " максимальное число.");
+
+
+// Задача 6:
+/* Напишите программу, которая на вход принимает число и выдаёт,
+ является ли число чётным (делится ли оно на два без остатка).*/
+
+// Console.Write("Введите ваше число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n % 2 == 0)
+// {
+//     Console.WriteLine(n + " является чётным.");
+// }
+// else
+// {
+//     Console.WriteLine(n + " является нечётным.");
+// }
+
+
+// Задача 8:
+/* Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N. */
+
+// Console.Write("Введите ваше число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+
+// while (count <= n)
+// {
+//     if (count % 2 == 0)
+//     {
+//         Console.WriteLine(count);
+//         count += 2;
+//     }
+//     else
+//     {
+//         count += 1;
+//     }
+// }
---
+++ Семинар/Lesson_2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+// if (number % 7 == 0)
+//     {
+//     if (number % 23 == 0)
+//         Console.WriteLine(number + " кратно 7 и 23.");
+//     else 
+//         Console.WriteLine("Условие не выполнено.");
+
+
+//     }
+// else   
+//     Console.WriteLine("Условие не выполнено.");
+
+
+/* Определить номер четверти плоскости,
+ в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0 */
+
+// Console.WriteLine("Введите X: ");
+// int x = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите Y: ");
+// int y = int.Parse(Console.ReadLine()!);
+
+
+// if (x > 0 && y > 0)
+
+//     Console.WriteLine("Первая четверть.");
+
+// else if (x < 0 && y > 0)
+
+//     Console.WriteLine("Вторая четверть.");
+
+// else if (x < 0 && y < 0)
+
+//     Console.WriteLine("Третья четверть.");
+
+// else if (x > 0 && y < 0)
+
+//     Console.WriteLine("Четвертая четверть.");
+
+// else
+//     Console.WriteLine("Ввели 0");
+
+/* Программа проверяет
+ пятизначное число на палиндромом.*/
+
+Console.WriteLine("Введите пятизначное число.");
+
+// string? num = Console.ReadLine();
+
+// if (num[0] == num[4] && num[1] == num[3])
+
+//     Console.WriteLine("Верно");
+
+// else
+//     Console.WriteLine("не Верно");
