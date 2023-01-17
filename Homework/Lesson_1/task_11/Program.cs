4ce4b4db87627b3b6c730ccf3eb3df09ddf9b48e
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-17 19:47:17 +0300

    Added task_11

---
+++ Homework/Lesson_1/task_11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB % numberA == 0)
+    Console.WriteLine(numberB + " кратно " + numberA);
+else
+    Console.WriteLine(numberB % numberA + " остаток.");
+
+//Console.WriteLine(number);
+
