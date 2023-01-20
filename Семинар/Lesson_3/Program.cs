431ce9b00ba30b079377965b7d1980590687d76e
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-20 21:58:22 +0300

    Записал семинар 3

---
+++ Семинар/Lesson_3/Program.cs
@@ -0,0 +1,56 @@
+﻿// 
+
+// Console.WriteLine("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+
+// int[] array = new int[n];
+
+// Console.WriteLine(array[0]);
+
+
+// Найти сумму чисел от 1 до А
+
+// Console.WriteLine("Введите число: ");
+// int A = int.Parse(Console.ReadLine()!);
+// int sum = 0;
+// for (int i = 0; i <= A; i++)
+// {
+//     sum += i;
+// }
+    
+// Console.WriteLine(sum);
+
+
+// Возведите число А в натуральную степень B используя цикл
+
+// Console.WriteLine("Введите число: ");
+// int A = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите число: ");
+// int B = int.Parse(Console.ReadLine()!);
+
+// int result = 1;
+// for (int i = 1; i <= B; i++)
+// { 
+//     result *= A; 
+// }
+// Console.WriteLine(result);
+
+
+// Определить количество цифр в числе
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+// int count = 0;
+
+// while (number > 0)
+// {
+//     number = number / 10;
+//     count++;
+// }
+// Console.WriteLine(count);
+
+
+// 
+
