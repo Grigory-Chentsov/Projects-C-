dd389516a91aaed2f5491a8bcd03f47cdd7a1b01
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-20 19:53:43 +0300

    Решение задачи 3

---
+++ Homework/Lesson_2/task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер интересующей четверти: ");
+int quarter_number = int.Parse(Console.ReadLine()!);
+
+string[] quarters = { "x, y", "-x, y", "-x, -y", "x, -y" };
+int index = quarter_number - 1;
+
+if (quarter_number <= 4)
+{
+    Console.WriteLine(quarters[index]);
+}
+else 
+    Console.WriteLine("Error: Выберите одну из четырёх четвертей.");
+
