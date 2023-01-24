6e878758b6c407db045fb826a4180264b7e985e2
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-24 19:07:25 +0300

    Added gitignore lesson 3

---
+++ Homework/Lesson_3/task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найдите кубы чисел от 1 до N;
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    int cube = i * i * i;
+    Console.WriteLine(cube);
+}
