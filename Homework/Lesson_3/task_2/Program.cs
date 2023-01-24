f9e386d3b278f86f892e4c320efd575d2023929b
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-24 19:46:08 +0300

    ДЗ 3 Решение задачи 2

---
+++ Homework/Lesson_3/task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Посчитать сумму чисел в числе
+
+Console.WriteLine("Введите число: ");
+string input = Console.ReadLine();
+int len = input.Length;
+int number = int.Parse(input);
+int sum = 0; 
+
+for (int i = 0; i <= len; i++)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+}
+
+Console.WriteLine(sum);
