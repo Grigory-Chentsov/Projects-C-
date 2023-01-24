e54f9adbda2cd068c9a780f61392276b86c396f4
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-24 20:12:35 +0300

    ДЗ 3 Решение задачи 3

---
+++ Homework/Lesson_3/task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int Multiplication(int count)
+{
+    int result = 1;
+    
+    for (int i = 1; i <= count; i++)
+    {
+        result = result * i;
+        
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int count = int.Parse(Console.ReadLine()!);
+
+int res = Multiplication(count);
+Console.WriteLine(res);
+
+
+
