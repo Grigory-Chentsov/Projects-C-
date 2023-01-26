c5fe88cdf7779401a3506e5ad8b7691333f55b74
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-26 11:50:10 +0300

    Решение задачи 4 Lesson 3

---
+++ Homework/Lesson_3/task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Поиск кубов последовательных чисел, с окончанием на чётную цифру.");
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+var numbers = new List<int>();
+
+for (int i = 0; i <= N; i++)
+{
+    int cube = i * i * i;
+    int last_num = cube % 10;
+    if (last_num % 2 == 0)
+    {
+        numbers.Add(cube);
+    }
+    Console.WriteLine(cube);
+    
+}
+Console.WriteLine(string.Join(", ", numbers));
+
+
+
