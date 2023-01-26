dc7723e1f3c4d6b882a5c942c53c33aa8b64949d
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-26 12:50:22 +0300

    Решение задачи 1, lesson 4

---
+++ Homework/Lesson_4/task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void ZerOne(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+
+    }
+}
+
+void print_ZerOne(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.WriteLine(array[i]);
+
+    }
+}
+
+
+int[] array = new int[8];
+
+ZerOne(array);
+print_ZerOne(array);
+// Console.WriteLine(string.Join(", ", array));
