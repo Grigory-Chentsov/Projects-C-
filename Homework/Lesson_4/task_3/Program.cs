81b067dcf89aa02815069adc0f35169376446764
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-27 19:43:23 +0300

    Решение задачи 3, lesson 4

---
+++ Homework/Lesson_4/task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задать массив, заполнить случайными положительными трёхзначными числами.
+   Показать количество нечетных\четных чисел */
+
+Console.WriteLine("Сколько элементов должно быть в массиве?");
+int amount = int.Parse(Console.ReadLine()!);
+
+void FillArray(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+void PrintNumber(int[] array)
+{
+    int length = array.Length;
+    int even = 0;
+    int odd = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] % 2 == 0)
+            even += 1;
+        else
+            odd += 1;
+    }
+    Console.WriteLine($"В массиве {even} чётных и {odd} нечётных чисел.");
+}
+
+int[] array = new int[amount];
+FillArray(array);
+PrintNumber(array);
+
+Console.WriteLine(string.Join(", ", array));
