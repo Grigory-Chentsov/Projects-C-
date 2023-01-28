89f85d9c743028e9de6f5fbf83308cdf685f4435
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-28 13:53:03 +0300

    Решение задачи 1, lesson 5

--- Homework/Lesson_5/task_1/Program.cs
+++ Homework/Lesson_5/task_1/Program.cs
@@ -1,8 +1,28 @@
 ﻿/* Найти точку пересечения двух прямых заданных уравнением
    y = k1 * x + b1, y = k2 * x + b2. b1, k1 и b2, k2 заданы */
 
+double k1 = 39;
+double b1 = 12;
+double b2 = 32;
+double k2 = 34;
 
+double x = 0;
+double y = 0;
 
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Прямые совпадают.");
+    return;
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны.");
+    return;
+}
+else
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+}
 
-
-Console.WriteLine("Hello, World!");
+Console.WriteLine($"{x} {y}");
