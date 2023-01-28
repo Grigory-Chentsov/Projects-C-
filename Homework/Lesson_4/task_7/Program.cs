540c5a616d53c2580eafb4ef9d999b2f8db791ed
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-28 13:06:16 +0300

    Решение задачи 7, lesson 4

---
+++ Homework/Lesson_4/task_7/Program.cs
@@ -0,0 +1,22 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+double[] array = { 15, 56.22, 0.0, 12423, -232336.11, 0, 1, -1, 2.245, 845 };
+
+double min = 0;
+double max = 0;
+
+void Find_Min_Max(double[] aray)
+{
+    int len = array.Length;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (array[i] < min) min = array[i];
+        else if (array[i] > max) max = array[i];
+    }
+}
+
+Find_Min_Max(array);
+double diff = max - min;
+
+Console.WriteLine($"Разница между максимальным {max} и минимальным {min} элементами \nравна {diff}");
