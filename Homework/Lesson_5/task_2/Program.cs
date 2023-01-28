7e05beac8eeae10b28a9046fec04c3ef626e65bb
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-29 00:15:37 +0300

    Решение задачи 2, lesson 5

---
+++ Homework/Lesson_5/task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Показать числа Фибоначчи
+
+Console.WriteLine("Введите число для поиска последовательности Фибоначчи: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int num0 = 0;
+int num1 = 1;
+int temp;
+
+for (int i = 0; i <= number; i++)
+{
+    if (number == 0 || number == 1)
+        Console.WriteLine(number);
+    else
+    {
+        Console.Write($"{num1} ");
+        temp = num1;
+        num1 = num1 + num0;
+        num0 = temp;
+    }
+
+}
+
+
+
