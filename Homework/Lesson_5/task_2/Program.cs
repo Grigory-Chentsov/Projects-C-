fa5d96a3fc24e5cb132f0f455fba98ff72831801
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-29 19:50:18 +0300

    Решение task_2 с помощью рекурсии

--- Homework/Lesson_5/task_2/Program.cs
+++ Homework/Lesson_5/task_2/Program.cs
@@ -3,23 +3,33 @@
 Console.WriteLine("Введите число для поиска последовательности Фибоначчи: ");
 int number = int.Parse(Console.ReadLine()!);
 
-int num0 = 0;
-int num1 = 1;
-int temp;
-
-for (int i = 0; i <= number; i++)
+double Fibonacci(int number)
 {
-    if (number == 0 || number == 1)
-        Console.WriteLine(number);
-    else
-    {
-        Console.Write($"{num1} ");
-        temp = num1;
-        num1 = num1 + num0;
-        num0 = temp;
-    }
-
+    if (number == 1 || number == 0) return number;
+    else return Fibonacci(number - 1) + Fibonacci(number - 2);
 }
+for (int i = 1; i < number; i++)
+{
+    Console.Write($"{Fibonacci(i)} ");
+}
+
+// int num0 = 0;
+// int num1 = 1;
+// int temp;
+
+// for (int i = 0; i <= number; i++)
+// {
+//     if (number == 0 || number == 1)
+//         Console.WriteLine(number);
+//     else
+//     {
+//         Console.Write($"{num1} ");
+//         temp = num1;
+//         num1 = num1 + num0;
+//         num0 = temp;
+//     }
+
+// }
 
 
 
