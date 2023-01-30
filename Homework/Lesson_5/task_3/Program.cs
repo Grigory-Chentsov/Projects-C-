2aa240ec634a8de002ad716689a4aca99cbc7e3d
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-01-30 14:05:48 +0300

    Решение задачи 3, lesson 5

--- Homework/Lesson_5/task_3/Program.cs
+++ Homework/Lesson_5/task_3/Program.cs
@@ -1,7 +1,39 @@
-﻿// Написать программу масштабирования фигуры
+﻿/* Написать программу масштабирования фигуры
+   Тут для тех кто далеко улетел, чтобы задавались вершины фигуры списком (одной строкой)
+   например: "(0,0) (2,0) (2,2) (0,2)"
+   коэффициент масштабирования k задавался отдельно - 2 или 4 или 0.5
+   В результате показать координаты, которые получатся.
+   при k = 2 получаем "(0,0) (4,0) (4,4) (0,4)" */
+
+Console.WriteLine("Введите координаты вершин: ");
+string? coordinates = Console.ReadLine();
+
+string StringReplace(ref string coordinates)
+{
+    coordinates = coordinates.Replace(",", " ");
+    coordinates = coordinates.Replace("(", " ");
+    coordinates = coordinates.Replace(")", " ");
+    coordinates = coordinates.Replace(" ", "");
+    coordinates = coordinates.TrimStart();
+    return coordinates;
+}
+
+StringReplace(ref coordinates);
+
+Console.WriteLine("Введите коэффициент масштабирования: ");
+double k = double.Parse(Console.ReadLine()!);
+
+double[] new_coord = new double[coordinates.Length];
+
+for (int i = 0; i < coordinates.Length; i++)
+{
+    new_coord[i] = ((double)coordinates[i] - 48) * k;
+}
+Console.WriteLine($"Координаты расширения ({new_coord[0]},{new_coord[1]})"
+                                       + $" ({new_coord[2]},{new_coord[3]})"
+                                       + $" ({new_coord[4]},{new_coord[5]})"
+                                       + $" ({new_coord[6]},{new_coord[7]})");
 
 
 
 
-
-Console.WriteLine("Hello, World!");
