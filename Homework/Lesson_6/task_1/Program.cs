6f00b9ff89de1acdfc69e6e494f1ebab3d811035
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-02-01 22:18:08 +0300

    Решение задачи 1, lesson 6

---
+++ Homework/Lesson_6/task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Показать двумерный массив размером m×n заполненный вещественными числами */
+
+Console.Write("Введите колличество строк: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите колличество столбцов: ");
+int n = int.Parse(Console.ReadLine()!);
+
+void PrintTable(double[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"{table[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] FillTable(double[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = Math.Round(new Random().NextDouble() * 20 - 10, 3);
+        }
+    }
+    return table;
+}
+
+double[,] table = new double[m, n];
+FillTable(table);
+PrintTable(table);
