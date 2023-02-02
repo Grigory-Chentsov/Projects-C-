1944e189679b1757048c78b9e9ca24f7fe246356
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-02-02 19:14:40 +0300

    Решение задачи 2, lesson 6

---
+++ Homework/Lesson_6/task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задать двумерный массив следующим правилом: Aₘₙ = m+n */
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+void PrintArray(int[,] table)
+{
+    for (int str = 0; str < table.GetLength(0); str++) 
+    {
+        for (int col = 0; col < table.GetLength(1); col++)
+        {
+            Console.Write($" {table[str, col]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = i + j;
+        }
+    }
+    return table;
+}
+
+int[,] table = new int[m, n];
+FillArray(table);
+PrintArray(table);
+
+
