e2d227dc224ce53542c9b821d69400b9a845dc7d
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-02-15 00:51:10 +0300

    Решение задачи 2, project_final

---
+++ Homework/project_final/task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Показать натуральные числа от M до N, N и M заданы */
+
+int M = 18;
+int N = 12;
+
+void MmoreNPrint(int M, int N)
+{
+    if (M >= N)
+    {
+        Console.Write(M + " ");
+        MmoreNPrint(M - 1, N);
+    }
+}
+
+void MlessNPrint(int M, int N)
+{
+    if (M <= N)
+    {
+        MlessNPrint(M, N - 1);
+        Console.Write(N + " ");
+    }
+
+}
+
+if(M > N) MmoreNPrint(M, N);
+else MlessNPrint(M, N);
