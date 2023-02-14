f61853a0e53902e3f98381ae2976fa56fc05f187
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-02-14 23:49:40 +0300

    Решение задачи 1, project_final

---
+++ Homework/project_final/task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Показать натуральные числа от N до 1, N задано */
+
+int N = 8;
+
+void PrintNumber(int N)
+{
+    if(N < 1) return;
+    Console.Write(N + " ");
+    PrintNumber(N - 1);    
+}
+
+PrintNumber(N);
