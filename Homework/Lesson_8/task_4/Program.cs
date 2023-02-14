f8591c4c577aed6e6800b5259a4ee2e5ba723b48
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-02-14 17:51:45 +0300

    Решение задачи 4, lesson 8

---
+++ Homework/Lesson_8/task_4/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Сформировать трехмерный массив не повторяющимися двузначными числами 
+показать его построчно на экран выводя индексы соответствующего элемента*/
+
+HashSet<int> numbers = new HashSet<int>();
+int GenerateUniqueNumber()
+{
+    while (true)
+    {
+        Random r = new Random();
+        var n = r.Next(10, 99);
+        if (!numbers.Contains(n))
+        {
+            numbers.Add(n);
+            return n;
+
+        }
+        if (numbers.Count >= 89)
+            return n;
+    }
+}
+
+int[,,] arr = new int[3, 3, 3];
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        for (int k = 0; k < 3; k++)
+        {
+            arr[i, j, k] = GenerateUniqueNumber();
+            Console.WriteLine($"{arr[i, j, k]} индекс {i} {j} {k}");
+        }
+    }
+}
+
