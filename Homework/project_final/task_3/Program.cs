ac7d07903dee981299eb0e0dd03a724ba297669a
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-02-15 21:08:50 +0300

    Решение задачи 3, project_final

---
+++ Homework/project_final/task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Найти сумму элементов от M до N, N и M заданы */
+
+int SumNumbersLess(int m, int n)
+{  
+    if(m > n) return 0;    
+    return m + SumNumbersLess(m + 1, n);      
+}
+
+int SumNumbersMore(int m, int n)
+{  
+    if(m < n) return 0;    
+    return m + SumNumbersMore(m - 1, n);      
+}
+
+int m = 2;
+int n = 5;
+
+if(m > n) Console.WriteLine(SumNumbersMore(m, n));
+else Console.WriteLine(SumNumbersLess(m, n));
+
