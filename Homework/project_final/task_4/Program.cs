5d983a24a2c00e93224654a45ac2c96c569101ed
Grigory Chentsov <cencovgrigor@yandex.ru>
2023-02-15 22:38:18 +0300

    Решение задачи 4, project_final

---
+++ Homework/project_final/task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Написать программу вычисления функции Аккермана */
+
+int FuncAck(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return FuncAck(m - 1, 1);
+    }
+    else return FuncAck(m - 1, FuncAck(m, n - 1));
+}
+
+int m = 1;
+int n = 1;
+
+if(m > 3 || n > 4 || m < 0 || n < 0) Console.WriteLine("Stack overflow.");
+else Console.WriteLine(FuncAck(m, n));
+
